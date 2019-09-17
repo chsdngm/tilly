a9b385d562149811e282daf7f4492fc6c3eb305c
Bespalov Konstantin <k.bespalov@tinkoff.ru>
2019-09-17 16:50:23 +0300

    future flow

--- src/main/java/com/imbananko/tilly/MemeManager.java
+++ src/main/java/com/imbananko/tilly/MemeManager.java
@@ -21,6 +21,7 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
 
 import static com.imbananko.tilly.model.VoteEntity.Value.*;
@@ -68,7 +69,7 @@ public class MemeManager extends TelegramLongPollingBot {
             Case($(), () -> null));
   }
 
-  private MemeEntity processMeme(Update update) {
+  private Void processMeme(Update update) {
     Message message = update.getMessage();
     MemeEntity meme =
         MemeEntity.builder()
@@ -77,79 +78,88 @@ public class MemeManager extends TelegramLongPollingBot {
             .fileId(message.getPhoto().get(0).getFileId())
             .build();
 
-    Try.of(
-            () ->
-                execute(
-                    new SendPhoto()
-                        .setChatId(chatId)
-                        .setPhoto(meme.getFileId())
-                        .setCaption("Sender: " + meme.getAuthorUsername())
-                        .setReplyMarkup(createMarkup(meme))))
-        .onSuccess(ignore -> log.info("Sent meme=" + meme))
-        .onFailure(
-            throwable ->
-                log.error("Failed to send meme=" + meme + ". Exception=" + throwable.getMessage()));
 
-    return memeRepository.save(meme);
+    CompletableFuture.supplyAsync(() -> Try.of(() -> execute(
+            new SendPhoto()
+                    .setChatId(chatId)
+                    .setPhoto(meme.getFileId())
+                    .setCaption("Sender: " + meme.getAuthorUsername())
+                    .setReplyMarkup(createMarkup(meme)))
+    )).thenCompose(res -> {
+            res
+              .onSuccess(ignore -> log.info("Sent meme=" + meme))
+              .onFailure(
+                      throwable ->
+                              log.error("Failed to send meme=" + meme + ". Exception=" + throwable.getMessage()));
+
+      return CompletableFuture.supplyAsync(() -> memeRepository.save(meme));
+    });
+
+    return null;
   }
 
-  private VoteEntity processVote(Update update) {
+  private Void processVote(Update update) {
     final Message message = update.getCallbackQuery().getMessage();
-    final MemeEntity meme =
-        memeRepository.findById(message.getPhoto().get(0).getFileId()).orElseThrow();
+    final CompletableFuture<MemeEntity> memeFuture =
+            CompletableFuture.supplyAsync(() -> memeRepository.findById(message.getPhoto().get(0).getFileId()).orElseThrow());
 
-    VoteEntity voteEntity =
-        VoteEntity.builder()
-            .chatId(chatId)
-            .fileId(meme.getFileId())
-            .username(update.getCallbackQuery().getFrom().getUserName())
-            .value(VoteEntity.Value.valueOf(update.getCallbackQuery().getData()))
-            .build();
+    memeFuture
+            .thenCompose(meme -> {
+              VoteEntity voteEntity =
+                      VoteEntity.builder()
+                              .chatId(chatId)
+                              .fileId(meme.getFileId())
+                              .username(update.getCallbackQuery().getFrom().getUserName())
+                              .value(VoteEntity.Value.valueOf(update.getCallbackQuery().getData()))
+                              .build();
 
-    if (voteRepository.exists(voteEntity)) {
-      voteRepository.delete(voteEntity);
-    } else {
-      voteRepository.save(voteEntity);
-    }
+              return CompletableFuture.supplyAsync(() -> {
+                if (voteRepository.exists(voteEntity)) {
+                  voteRepository.delete(voteEntity);
+                } else {
+                  voteRepository.save(voteEntity);
+                }
 
-    Try.of(
-            () ->
-                execute(
-                    new EditMessageReplyMarkup()
-                        .setMessageId(message.getMessageId())
-                        .setChatId(message.getChatId())
-                        .setInlineMessageId(update.getCallbackQuery().getInlineMessageId())
-                        .setReplyMarkup(createMarkup(meme))))
-        .onSuccess(ignore -> log.info("Updated meme=" + meme))
-        .onFailure(
-            throwable ->
-                log.error(
-                    "Failed to update meme=" + meme + ". Exception=" + throwable.getMessage()));
+                return null;
+              }).thenCompose(notUsed -> CompletableFuture.supplyAsync(() -> Try.of(
+                      () ->
+                              execute(
+                                      new EditMessageReplyMarkup()
+                                              .setMessageId(message.getMessageId())
+                                              .setChatId(message.getChatId())
+                                              .setInlineMessageId(update.getCallbackQuery().getInlineMessageId())
+                                              .setReplyMarkup(createMarkup(meme))))
+                      .onSuccess(ignore -> log.info("Updated meme=" + meme))
+                      .onFailure(
+                              throwable ->
+                                      log.error(
+                                              "Failed to update meme=" + meme + ". Exception=" + throwable.getMessage()))));
+            });
 
-    if (VoteEntity.Value.valueOf(update.getCallbackQuery().getData()).equals(EXPLAIN)) {
-      final var explainCount =
-          voteRepository.countByFileIdAndChatIdAndValue(meme.getFileId(), meme.getTargetChatId(), EXPLAIN);
+//    if (VoteEntity.Value.valueOf(update.getCallbackQuery().getData()).equals(EXPLAIN)) {
+//      final var explainCount =
+//          voteRepository.countByFileIdAndChatIdAndValue(meme.getFileId(), meme.getTargetChatId(), EXPLAIN);
+//
+//      if (explainCount == 2) {
+//        final var replyText =
+//            "@" + update.getCallbackQuery().getMessage().getCaption().replaceFirst("Sender: ", "")
+//                + ", поясни за мем";
+//        Try.of(
+//                () ->
+//                    execute(
+//                        new SendMessage()
+//                            .setChatId(message.getChatId())
+//                            .setReplyToMessageId(update.getCallbackQuery().getMessage().getMessageId())
+//                            .setText(replyText)))
+//            .onSuccess(ignore -> log.info("Successful reply for explaining"))
+//            .onFailure(
+//                throwable ->
+//                    log.error(
+//                        "Failed to reply for explaining. Exception=" + throwable.getMessage()));
+//      }
+//    }
 
-      if (explainCount == 2) {
-        final var replyText =
-            "@" + update.getCallbackQuery().getMessage().getCaption().replaceFirst("Sender: ", "")
-                + ", поясни за мем";
-        Try.of(
-                () ->
-                    execute(
-                        new SendMessage()
-                            .setChatId(message.getChatId())
-                            .setReplyToMessageId(update.getCallbackQuery().getMessage().getMessageId())
-                            .setText(replyText)))
-            .onSuccess(ignore -> log.info("Successful reply for explaining"))
-            .onFailure(
-                throwable ->
-                    log.error(
-                        "Failed to reply for explaining. Exception=" + throwable.getMessage()));
-      }
-    }
-
-    return voteEntity;
+    return null;
   }
 
   private InlineKeyboardMarkup createMarkup(MemeEntity memeEntity) {
